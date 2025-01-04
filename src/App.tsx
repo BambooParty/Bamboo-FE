3197f18aefc1e8fd0310769cad0b73b6e9cb077e
hyewon-p <hyewon.parkhw@gmail.com>
2025-01-04 13:05:49 +0900

    add: login page layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
         </Routes>
       </BrowserRouter>
     </>
