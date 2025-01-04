7147e3017441ab260cc796d20c45355c8ca41f42
jihyun-j <jihyun9142@gmail.com>
2025-01-04 10:42:43 +0900

    chore: 프로젝트 초기설정

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-h2">hello</h1>
+      <Button>click</Button>
+    </>
+  );
+}
+
+export default App;
