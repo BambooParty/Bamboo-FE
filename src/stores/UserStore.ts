4bd3f7f1f7625c09c8a39d9166d52dec812a4bf3
hyewon-p <hyewon.parkhw@gmail.com>
2025-01-05 17:06:16 +0900

    add: 번역 기능

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -1,10 +1,11 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
 // 사용자 상태 타입
 interface User {
   userId: string;
   nickname: string;
+  mbti: string;
+  chatId: string;
 }
 
 // Zustand 스토어 타입
@@ -12,20 +13,18 @@ interface UserStore {
   user: User | null; // 사용자 정보
   setUser: (user: User) => void; // 사용자 정보 설정
   clearUser: () => void; // 사용자 정보 초기화
+  updateUserField: (field: Partial<User>) => void;
 }
 
 // Zustand 스토어 생성
-const useUserStore = create<UserStore>()(
-  persist(
-    (set) => ({
-      user: null,
-      setUser: (user) => set({ user }),
-      clearUser: () => set({ user: null }),
-    }),
-    {
-      name: "user-storage", // 로컬스토리지 키 이름
-    }
-  )
-);
+const useUserStore = create<UserStore>()((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+  clearUser: () => set({ user: null }),
+  updateUserField: (field) =>
+    set((state) => ({
+      user: state.user ? { ...state.user, ...field } : null,
+    })),
+}));
 
 export default useUserStore;
