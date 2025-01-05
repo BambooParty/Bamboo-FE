3450cc7b328856d25a4896e6bc110447c5d71838
jihyun-j <jihyun9142@gmail.com>
2025-01-05 11:53:07 +0900

    feat: 프로필 헤더 추가

--- src/components/Profile/ProfileHeader.tsx
+++ src/components/Profile/ProfileHeader.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import Avatar from "boring-avatars";
+import MbtiBadge from "../MbtiBadge";
+import { Button } from "../ui/button";
 
-function ProfileHeader() {
-  return <div>ProfileHeader</div>;
+interface ProfileHeaderProps {
+  username: string;
+  userId: string;
+  mbti: string;
 }
+
+const ProfileHeader: React.FC<ProfileHeaderProps> = ({
+  username,
+  userId,
+  mbti,
+}) => {
+  return (
+    <div className="flex justify-between items-center">
+      <div className="flex items-center gap-3">
+        <Avatar name={username} variant="beam" size={100} />
+        <div className="flex">
+          <div className="flex flex-col items-start">
+            <div className="flex gap-2">
+              <h2 className="text-2xl font-bold">{username}</h2>
+              <MbtiBadge mbti={mbti} />
+            </div>
+            <span>{userId}</span>
+          </div>
+        </div>
+      </div>
+      <Button>프로필 수정</Button>
+    </div>
+  );
+};
 
 export default ProfileHeader;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,21 @@
 import Chart from "@/components/Profile/Chart";
+import ProfileHeader from "@/components/Profile/ProfileHeader";
 import React from "react";
 
+const userData = {
+  id: "1",
+  userId: "user1", // 가입한 id
+  username: "전지현", // 닉네임
+  mbti: "INTP",
+};
 const Profile = () => {
   return (
-    <div>
+    <div className="w-full">
+      <ProfileHeader
+        username={userData.username}
+        mbti={userData.mbti}
+        userId={userData.userId}
+      />
       <Chart />
     </div>
   );
